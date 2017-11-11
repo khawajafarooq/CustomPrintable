c9062cdf2e8d8c77a42a320ace71ed78572bade3
Khawaja Farooq <kfarooqa@gmail.com>
2017-11-11 08:20:03 +0300

    adding comments

--- CustomPrintable.playground/Contents.swift
+++ CustomPrintable.playground/Contents.swift
@@ -1,12 +1,14 @@
-import UIKit
+import Foundation
 
 
+// struct example of Custom Printable
 struct User {
     let firstName: String
     let lastName: String
     let age: Int
 }
 
+// Pretty Printable of User
 extension User: CustomStringConvertible {
     var description: String {
         return "👤 Name: \(firstName) \(lastName), Age: \((age))"
@@ -15,4 +17,6 @@ extension User: CustomStringConvertible {
 
 let user = User(firstName: "John", lastName: "Doe", age: 22)
 print(user)
+
+
 
