b8c14fd4dce18ff7ac4023db88865049dd0fb10b
Khawaja Farooq <kfarooqa@gmail.com>
2017-11-14 10:32:15 +0300

    Layman example

--- CustomPrintable.playground/Contents.swift
+++ CustomPrintable.playground/Contents.swift
@@ -1,51 +1,6 @@
 import UIKit
 
-// Selling the idea of pretty printable
-
-protocol CustomPrintable: CustomStringConvertible {
-    var emoji: String { get }
-}
-
-extension CustomPrintable {
-
-    var emoji: String {
-        return "📝"
-    }
-
-    var description: String {
-
-        var desc: String = "\(type(of: self)) \(emoji)\n"
-        let object = Mirror(reflecting: self)
-
-        for property in object.children {
-            guard let propertyName = property.label else {
-                continue
-            }
-
-            desc += "\(propertyName): \(property.value)\n"
-        }
-        return desc
-    }
-}
-
-// Custom Printable value type struct
-struct User {
-    let firstName: String
-    let lastName: String
-    let age: Int
-}
-
-extension User: CustomPrintable {
-    var emoji: String {
-        return "👨🏼‍🚀"
-    }
-}
-
-let user = User(firstName: "John", lastName: "Doe", age: 22)
-print(user)
-
-
-// CustomStringConvertible
+// Layman example of CustomStringConvertible
 struct AnotherUser {
     let name: String
     let gender: String
@@ -67,5 +22,47 @@ let aUser = AnotherUser(name: "Chris Angel", gender: "Male")
 print(aUser)
 
 
+// CustomPrintable pretty printable
+protocol CustomPrintable: CustomStringConvertible {
+    var emoji: String { get }
+}
+
+extension CustomPrintable {
+    
+    var emoji: String {
+        return "📝"
+    }
+    
+    var description: String {
+        
+        var desc: String = "\(type(of: self)) \(emoji)\n"
+        let object = Mirror(reflecting: self)
+        
+        for property in object.children {
+            guard let propertyName = property.label else {
+                continue
+            }
+            
+            desc += "\(propertyName): \(property.value)\n"
+        }
+        return desc
+    }
+}
+
+// Custom Printable value type struct
+struct User {
+    let firstName: String
+    let lastName: String
+    let age: Int
+}
+
+extension User: CustomPrintable {
+    var emoji: String {
+        return "👨🏼‍🚀"
+    }
+}
+
+let user = User(firstName: "John", lastName: "Doe", age: 22)
+print(user)
 
 
